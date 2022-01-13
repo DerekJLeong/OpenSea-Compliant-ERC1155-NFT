4d8a1d01702cf05f356f24b2999d70e59a977a2e
Derek J Leong <derekjleong@gmail.com>
2022-01-12 19:55:35 -0500

    Update ERC1155NFT.sol

--- ERC1155NFT.sol
+++ ERC1155NFT.sol
@@ -129,6 +129,11 @@ contract CupsOfJoe is ERC1155, Ownable, Pausable, ERC1155Supply {
         symbol = newSymbol;
     }
 
+    function withdraw() public onlyOwner {
+        (bool os, ) = payable(owner()).call{value: address(this).balance}("");
+        require(os);
+    }
+
 
     //
     //  Exposed Public Functions
